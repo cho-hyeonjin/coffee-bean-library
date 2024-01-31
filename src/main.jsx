e118ede1ffa54dc6e55947108910a0f03aba2653
cho-hyeonjin <gusls2005@gmail.com>
2024-01-31 19:10:50 +0900

    🏗️ feat: 루트 레지스트리의 첫번째 객체로 루트 경로 & 루트 컴포넌트 매핑하여 등록

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,11 @@ import Root from "./routes/root";
 
 // 2. createBrowserRouter 메서드의 파라미터로 route 객체를 담은 배열을 전달해서 router를 생성해준다.
 const router = createBrowserRouter([
+  // 3. 레지스트리에 첫번째 루트 객체를 등록한다.
   {
+    // 이 레지스트리 객체에서는 root 경로("/")에 <Root /> 컴포넌트가 보여지게 매핑한다.
     path: "/",
+    //
     element: <Root />,
   },
 ]);
