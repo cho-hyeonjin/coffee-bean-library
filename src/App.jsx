f21be0010110a774f7efa4c8de18ca1b5e238097
cho-hyeonjin <gusls2005@gmail.com>
2024-01-31 23:53:19 +0900

    ♻️refactor: App.jsx <nav> 요소 <Navbar /> 컴포넌트로 전환

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
-      <div id="navbar">
-        <h1>커피빈샵</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to={`/`}>홈 페이지</Link>
-            </li>
-            <li>
-              <Link to={`/products`}>전체 상품 페이지</Link>
-            </li>
-            <li>
-              <Link to={`/products/recommend`}>추천 상품 페이지</Link>
-            </li>
-            <li>
-              <Link to={`/products/:id`}>상품 상세 페이지</Link>
-            </li>
-            <li>
-              <Link to={`/cart`}>장바구니 페이지</Link>
-            </li>
-          </ul>
-        </nav>
-        {/* 하위 경로로 설정한 컴포넌트를 보여줄 html 위치에 Outlet 컴포넌트를 배치한다.*/}
-        {/* Outlet 컴포넌트는 root 경로 하위 경로에 설정한 컴포넌트를 렌더링 할 위치를 알려주는 React Router 제공 컴포넌트  */}
-        <div id="detail">
-          <Outlet />
-        </div>
-      </div>
+      <Navbar />
+      <Outlet />
     </>
   );
 }
