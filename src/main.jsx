b68f283062bdff8ae09623abd4fda21f30c6fc8b
cho-hyeonjin <gusls2005@gmail.com>
2024-01-31 16:33:50 +0900

    🏗️feat: BrowserRouter 메서드로 라우터 생성 & 첫번째 루트 객체 생성

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// 1. react-router-dom에서 createBrowserRouter 메서드와 RootProvider를 import해주고
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// 2. createBrowserRouter 메서드의 파라미터로 route 객체를 담은 배열을 전달해서 router를 생성해준다.
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* RouterProvider로 Unbrella Context를 생성하여 router를 prop으로 내려준다.*/}
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
