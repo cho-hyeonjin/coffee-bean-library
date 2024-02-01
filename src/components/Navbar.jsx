a51b126c59fd1fc1d08218631cdfa13139e8b22e
cho-hyeonjin <gusls2005@gmail.com>
2024-02-01 21:56:53 +0900

    🎨style: 글꼴 적용

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <header className="flex justify-between border-b border-gray-300 p-2">
-        <Link to="/" className="align-center items-center text-3xl">
-          <h1>커피빈 라이브러리</h1>
+      <header className="flex justify-between border-b border-gray-300 py-2 px-6">
+        <Link
+          to="/"
+          className="align-center items-center text-3xl font-semibold"
+        >
+          <h1>Coffee Bean Library</h1>
         </Link>
-        <nav className="flex align-center items-center gap-6">
-          <Link to="/products">🙋🏻‍♀️전체 상품</Link>
-          <Link to="/products/recommend">💁🏻‍♀️추천 상품</Link>
-          <Link to="/products/recommend">🏢입점 브랜드</Link>
+        <nav className="flex align-center items-center gap-40">
+          <Link to="/products">전체 상품</Link>
+          <Link to="/products/recommend">추천 상품</Link>
+          <Link to="/products/recommend">입점 브랜드</Link>
         </nav>
-        <nav className="flex align-center items-center gap-4">
-          <Link to="/cart">🛒장바구니</Link>
-          <Link to="/signup">📝가입</Link>
-          <Link to="/signin">🔐로그인</Link>
+        <nav className="flex align-center items-center gap-6">
+          <Link to="/cart">장바구니</Link>
+          <Link to="/signup">회원가입</Link>
+          <Link to="/signin">로그인</Link>
         </nav>
       </header>
     </>
