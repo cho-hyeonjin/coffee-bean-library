c833f9aa8a33f5505fa4b8e592f059e5fcdf3d26
cho-hyeonjin <gusls2005@gmail.com>
2024-02-01 18:40:53 +0900

    🎨style: layout 초안

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <header className="flex justify-between">
-        <Link to="/">
-          <h1>커피빈샵(🧭네비게이션바)</h1>
+      <header className="flex justify-between border-b border-gray-300 p-2">
+        <Link to="/" className="align-center items-center text-3xl">
+          <h1>커피빈 라이브러리</h1>
         </Link>
-        <nav>
+        <nav className="flex align-center items-center gap-6">
           <Link to="/products">🙋🏻‍♀️전체 상품</Link>
           <Link to="/products/recommend">💁🏻‍♀️추천 상품</Link>
+          <Link to="/products/recommend">🏢입점 브랜드</Link>
+        </nav>
+        <nav className="flex align-center items-center gap-4">
           <Link to="/cart">🛒장바구니</Link>
-          <button>가입</button>
-          <button>로그인</button>
+          <Link to="/signup">📝가입</Link>
+          <Link to="/signin">🔐로그인</Link>
         </nav>
       </header>
     </>
