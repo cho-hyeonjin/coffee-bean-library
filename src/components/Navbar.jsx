069b46206d6bc0451c0414408511f5e0afffd38d
cho-hyeonjin <gusls2005@gmail.com>
2024-02-03 10:22:58 +0900

    🎨style: 사용자 권한별 Navbar 메뉴 차등 노출

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,22 +25,28 @@ export default function Navbar() {
         </Link>
         <nav className="flex align-center items-center gap-16 text-sm shrink-0">
           <Link to="/about">사서 소개</Link>
-          <Link to="/products">전체 상품</Link>
-          <Link to="/products/recommend">추천 상품</Link>
-          <Link to="/products/brand">브랜드별 상품</Link>
+          <Link to="/products">전체 원두</Link>
+          <Link to="/products/recommend">이달의 추천 원두</Link>
+          <Link to="/products/brand">브랜드별 원두</Link>
         </nav>
         <nav className="flex align-center items-center gap-4 text-xs shrink-0">
           <Button text={"🔍"} />
-          {user && user.isAdmin && <Link to="/products/create">상품 생성</Link>}
+          {user && user.isAdmin && (
+            <Link to="/products/create">게시물 생성</Link>
+          )}
           <div className="flex align-center items-center gap-2 shrink-0">
             {!user && <Button text={"로그인"} onClick={logIn} />}
             {user && <User user={user} />}
             {user && <Button text={"로그아웃"} onClick={logOut} />}
           </div>
-          <Link to="/cart" className="flex">
-            <Button text={"장바구니"} />
-            <div className="rounded-full bg-black text-white px-1 mx-1">0</div>
-          </Link>
+          {user && (
+            <Link to="/cart" className="flex">
+              <Button text={"나의 관심 원두"} />
+              <div className="rounded-full bg-black text-white px-1 mx-1">
+                0
+              </div>
+            </Link>
+          )}
         </nav>
       </header>
     </>
