0526d1628206adbc1b059fb472b9df7305cd4bf8
cho-hyeonjin <gusls2005@gmail.com>
2024-02-04 19:57:26 +0900

    🐛fix: 루트 컴포넌트에 ContextProvider로 Umbrella Context 구성해주지 않아 발생한 에러

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./components/context/AuthContext";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <AuthContextProvider>
-      <Navbar />
-      <Outlet />
-    </AuthContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>
+        <Navbar />
+        <Outlet />
+      </AuthContextProvider>
+    </QueryClientProvider>
   );
 }
