39108fd26c3c240f68294318fb52aafc772b2b1f
cho-hyeonjin <gusls2005@gmail.com>
2024-02-05 03:35:56 +0900

    🏗️feat: 장바구니(나의 원두 서재) 아이템 수량 컴포넌트 생성, 적용

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 // import User from "./User";
 import Button from "./ui/Button";
 import { useAuthContext } from "../context/AuthContext";
+import CartStatus from "./CartStatus";
 
 export default function Navbar() {
   const { user, logIn, logOut } = useAuthContext();
@@ -14,30 +15,25 @@ export default function Navbar() {
         >
           <h1>Coffee Bean Library</h1>
         </Link>
-        <nav className="flex align-center items-center gap-16 text-sm shrink-0">
+        <nav className="flex align-center items-center gap-16 ml-20 text-sm shrink-0">
           <Link to="/about">사서 소개</Link>
           <Link to="/products">전체 원두</Link>
           <Link to="/products/recommend">이달의 추천 원두</Link>
           <Link to="/products/brand">브랜드별 원두</Link>
         </nav>
-        <nav className="flex align-center items-center gap-4 text-xs shrink-0">
-          <Button text={"🔍"} />
+        <nav className="flex align-center items-center gap-4 ml-20 text-xs shrink-0">
           {user && user.isAdmin && (
             <Link to="/products/create">게시물 생성</Link>
           )}
-          <div className="flex align-center items-center gap-2 shrink-0">
-            {!user && <Button text={"로그인"} onClick={logIn} />}
-            {/* {user && <User user={user} />} */}
-            {user && <Button text={"로그아웃"} onClick={logOut} />}
-          </div>
           {user && (
-            <Link to="/cart" className="flex">
-              <Button text={"나의 원두 서재"} />
-              <div className="rounded-full bg-black text-white px-1 mx-1">
-                0
-              </div>
+            <Link to="/cart">
+              <CartStatus />
             </Link>
           )}
+          <div className="">
+            {!user && <Button text={"로그인"} onClick={logIn} />}
+            {user && <Button text={"로그아웃"} onClick={logOut} />}
+          </div>
         </nav>
       </header>
     </>
