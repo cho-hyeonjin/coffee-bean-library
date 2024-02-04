431bdc6d30044112fbe89b8d20a5d55b4fbcf099
cho-hyeonjin <gusls2005@gmail.com>
2024-02-04 20:28:19 +0900

    🏗️ProductCard 컴포넌트 아웃라인 작성

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -11,7 +11,15 @@ export default function ProductCard({
 }) {
   return (
     <>
-      <li>{productName}</li>
+      <li>
+        <img src={image} alt={productName} />
+        <div>
+          <h3>브랜드명: {brandName}</h3>
+          <h3>제품명: {productName}</h3>
+          <p>타입: {blendType}</p>
+          <p>가격: {`${price} 원`}</p>
+        </div>
+      </li>
     </>
   );
 }
